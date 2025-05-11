f145a990b892f8aafc45559251a7fdca9fd56fd6
Trinh Hieu Phi <phitrinh367@gmail.com>
2025-05-11 07:32:19 +0100

    ADD: Docker, FIX: AI API

--- backend/src/main/java/com/example/demo/service/intface/IOpenAIService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.intface;
-
-import com.example.demo.model.immutableDTOs.Answer;
-import com.example.demo.model.immutableDTOs.Question;
-
-public interface IOpenAIService {
-    Answer getResult(Question question);
-}
-
