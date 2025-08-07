e3a2b9c6968422522d2b6d1eca9a09bd13ebf839
Trinh Hieu Phi <phitrinh367@gmail.com>
2025-08-07 13:39:15 +0700

    minor redis cache

--- backend/src/main/java/com/example/demo/model/io/response/object/UserStatsResponse.java
+++ backend/src/main/java/com/example/demo/model/io/response/object/UserStatsResponse.java
@@ -1,10 +1,14 @@
 package com.example.demo.model.io.response.object;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserStatsResponse {
     private long totalFlashcardSets;
     private long totalQuizSets;
